4b10b0176179ec3f0f09b26dd02bc587a3d9729e
Ethol Palmer <48893720+etholpalmer@users.noreply.github.com>
2022-04-08 17:44:11 -0400

    Minor changes

--- Get-CSVInfo.ps1
+++ Get-CSVInfo.ps1
@@ -3,9 +3,9 @@ param(
     [Parameter(Mandatory, ValueFromPipeline)]
     [Alias('Path')]
     [ValidateScript({
-        if(-not ($_ | Test-Path ))              { throw "File or folder does not exist!" }
-        if(-not ($_ | Test-Path -PathType Leaf  { throw "The Path argument must be a file, not a folder!" }
-        if($_.Attributes -match "Hidden"        { throw "The file cannot be hidden, indication it was already processed." }
+        if(-not ($_ | Test-Path ))                { throw "File or folder does not exist!" }
+        if(-not ($_ | Test-Path -PathType Leaf))  { throw "The Path argument must be a file, not a folder!" }
+        if($_.Attributes -match "Hidden")         { throw "The file cannot be hidden, indication it was already processed." }
         return $true
     })]
     [System.IO.FileInfo[]]$CSVFiles,
